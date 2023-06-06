cbd0f2d22bd0d8784877f5e0f49c1d7a959eb62b
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-06 14:20:49 -0500

    Added homepage about content, need to position

--- front-page.php
+++ front-page.php
@@ -1,20 +1,43 @@
 <?php get_header(); ?>
 
+<!-- Hero section -->
 <div class="hero">
-    <div class="hero__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/hero-image.png') ?>);">
+    <div class="hero__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/hero-image.png') ?>);">
         <div class="hero__content">
-            <div class="hero__signature-wrapper"><img class="hero__signature"
-                    src="<?php echo get_theme_file_uri('/images/signature.png') ?>" />
+            <div class="hero__signature-wrapper"><img class="hero__signature" src="<?php echo get_theme_file_uri('/images/signature.png') ?>" />
             </div>
-            <h1 class="hero__title">Elevate Your Career with Confidence</h1>
+            <h1 class="title title--lg">Elevate Your Career with Confidence</h1>
             <h2 class="hero__subheader">Guiding Ambitious Young Women to Success</h2>
-            <a class="btn btn--large btn--primary rounded" href="#">Ignite Your Potential Now</a>
+            <a class="btn btn--xl btn--primary rounded" href="#">Ignite Your Potential Now</a>
         </div>
     </div>
-
 </div>
-
+<!-- About section -->
+<section class="home-about">
+    <div class="home-about-content">
+        <!-- About image -->
+        <div class="home-about__img-wrapper">
+            <img class="home-about__img" src="<?php echo get_theme_file_uri('/images/hero-image.png') ?>" alt="Nancy sitting in her office full of plants smiling at the camera.">
+        </div>
+        <div class="home-about__content">
+            <h2 class="title title--md">Hi, I'm Nancy </h2>
+            <p class="home-about__text home-about__text--mgsm">Welcome! I'm, a professional coach based in the beautiful
+                state of Minnesota. With a
+                background in human
+                resources and a passion for empowering young female professionals, I am dedicated to helping you unlock
+                your
+                full potential and achieve your career goals.
+            </p>
+            <p class="home-about__text home-about__text--mglg">
+                I bring a deep understanding of organizational dynamics, workplace challenges, and effective strategies
+                for
+                career advancement. I'll share valuable insights and practical tools to navigate the corporate landscape
+                with confidence and authenticity.
+            </p>
+            <a class="btn btn--primary btn--large rounded" href="/about">My story</a>
+        </div>
+    </div>
+</section>
 <div class="full-width-split group">
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
@@ -40,31 +63,29 @@
 
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="#">
+                        <span class="event-summary__month"><?php
                                                             $eventDate = new DateTime(get_field('event_date'));
                                                             echo $eventDate->format('M');
                                                             ?></span>
-                    <span class="event-summary__day"><?php
+                        <span class="event-summary__day"><?php
                                                             echo $eventDate->format('d');
                                                             ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a
-                            href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php if (has_excerpt()) {
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php if (has_excerpt()) {
                                 echo get_the_excerpt();
                             } else {
                                 echo wp_trim_words(get_the_content(), 18);
                             } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    </div>
                 </div>
-            </div>
             <?php }
             ?>
 
-            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
-                    class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
         </div>
     </div>
@@ -78,21 +99,20 @@
 
             while ($homepagePosts->have_posts()) {
                 $homepagePosts->the_post(); ?>
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"><?php the_time('M'); ?></span>
-                    <span class="event-summary__day"><?php the_time('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a
-                            href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php if (has_excerpt()) {
+                <div class="event-summary">
+                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M'); ?></span>
+                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php if (has_excerpt()) {
                                 echo get_the_excerpt();
                             } else {
                                 echo wp_trim_words(get_the_content(), 18);
                             } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                    </div>
                 </div>
-            </div>
             <?php }
             wp_reset_postdata();
             ?>
@@ -109,8 +129,7 @@
 <div class="hero-slider">
     <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -119,8 +138,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -129,8 +147,7 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider__slide"
-                style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
+            <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
                 <div class="hero-slider__interior container">
                     <div class="hero-slider__overlay">
                         <h2 class="headline headline--medium t-center">Free Food</h2>
