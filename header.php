3776f7fec4c150d48f6a4c6cbc5336a95fb3727e
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-09 08:06:53 -0500

    Added social icons to header

--- header.php
+++ header.php
@@ -11,39 +11,51 @@
 <body>
     <?php $siteTitle = "Nancy Johnson"; ?>
     <header class="site-header">
-        <h2 class="logo">
-            <a class="logo__a" href="<?php echo site_url('/') ?>"><?php echo $siteTitle; ?></a>
-        </h2>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <nav class="nav">
-            <ul class="nav__ul">
-                <li class="nav__li" <?php
-                                    if (is_page('home') or wp_get_post_parent_id(0) === 22) echo 'class="nav__selected"'
-                                    ?>>
-                    <a class="nav__a" href="<?php echo site_url('/') ?>">Home</a>
-                </li>
-                <li class="nav__li" <?php
-                                    if (is_page('about') or wp_get_post_parent_id(0) === 5) echo 'class="nav__selected"'
-                                    ?>>
-                    <a class="nav__a" href="<?php echo site_url('/about') ?>">About</a>
-                </li>
-                <li class="nav__li" <?php
-                                    if (is_page('coaching') or wp_get_post_parent_id(0) === 14) echo 'class="nav__selected"'
-                                    ?>>
-                    <a class="nav__a" href="<?php echo site_url('/coaching') ?>">Coaching</a>
-                </li>
-                <li class="nav__li" <?php
-                                    if (is_page('contact') or wp_get_post_parent_id(0) === 16) echo 'class="nav__selected"'
-                                    ?>>
-                    <a class="nav__a" href="<?php echo site_url('/contact') ?>">Contact</a>
-                </li>
-                <li class="nav__li" <?php
-                                    if (is_page('blog') or wp_get_post_parent_id(0) === 16) echo 'class="nav__selected"'
-                                    ?>>
-                    <a class="nav__a" href="<?php echo site_url('/blog') ?>">Blog</a>
-                </li>
-
-            </ul>
-        </nav>
+        <div class="site-header__content">
+            <h2 class="logo">
+                <a class="logo__a" href="<?php echo site_url('/') ?>"><?php echo $siteTitle; ?></a>
+            </h2>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <nav class="nav">
+                <ul class="nav__ul">
+                    <li class="nav__li" <?php
+                                        if (is_page('home') or wp_get_post_parent_id(0) === 22) echo 'class="nav__selected"'
+                                        ?>>
+                        <a class="nav__a" href="<?php echo site_url('/') ?>">Home</a>
+                    </li>
+                    <li class="nav__li" <?php
+                                        if (is_page('about') or wp_get_post_parent_id(0) === 5) echo 'class="nav__selected"'
+                                        ?>>
+                        <a class="nav__a" href="<?php echo site_url('/about') ?>">About</a>
+                    </li>
+                    <li class="nav__li" <?php
+                                        if (is_page('coaching') or wp_get_post_parent_id(0) === 14) echo 'class="nav__selected"'
+                                        ?>>
+                        <a class="nav__a" href="<?php echo site_url('/coaching') ?>">Coaching</a>
+                    </li>
+                    <li class="nav__li" <?php
+                                        if (is_page('contact') or wp_get_post_parent_id(0) === 16) echo 'class="nav__selected"'
+                                        ?>>
+                        <a class="nav__a" href="<?php echo site_url('/contact') ?>">Contact</a>
+                    </li>
+                    <li class="nav__li" <?php
+                                        if (is_page('blog') or wp_get_post_parent_id(0) === 16) echo 'class="nav__selected"'
+                                        ?>>
+                        <a class="nav__a" href="<?php echo site_url('/blog') ?>">Blog</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header__icons-wrapper">
+                <a href="#" class="header__icon-wrapper">
+                    <i class="fa-brands fa-instagram header__icon"></i>
+                </a>
+                <a href="#" class="header__icon-wrapper">
+                    <i class="fa-brands fa-facebook-f header__icon"></i>
+                </a>
+                <a href="#" class="header__icon-wrapper">
+                    <i class="fa-brands fa-linkedin-in header__icon"></i>
+                </a>
+            </div>
+        </div>
 
     </header>
