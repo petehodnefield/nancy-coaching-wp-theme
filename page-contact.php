d54b2ce7e97d3afaee9042572c1430b924e834d4
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-12 09:22:50 -0500

    Contact page optimized for small screens

--- page-contact.php
+++ page-contact.php
@@ -5,47 +5,29 @@ get_header();
 while (have_posts()) {
     the_post(); ?>
 
-
-
-<div class="container container--narrow page-section">
-    <h2>Contact</h2>
-    <?php
-        $parentPage = wp_get_post_parent_id(get_the_id());
-
-
-        ?>
-
-
-
-    <?php
-        $testArray = get_pages(array(
-            'child_of' => get_the_ID()
-        ));
-
-        if ($parentPage or $testArray) { ?>
-
-    <div class="page-links">
-        <h2 class="page-links__title"><a
-                href="<?php echo get_permalink($parentPage); ?>"><?php echo get_the_title($parentPage); ?></a></h2>
-        <ul class="min-list">
-            <?php
-                    if ($parentPage) {
-                        $findChildrenOf = $parentPage;
-                    } else {
-                        $findChildrenOf = get_the_ID();
-                    }
-                    wp_list_pages(array(
-                        'title_li' => NULL,
-                        'child_of' => $findChildrenOf,
-                        'sort_column' => 'menu_order'
-                    ));
-                    ?>
-        </ul>
+<div class="page-banner page-banner--contact">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/hero-image.png') ?>);"></div>
+    <div class="page-banner__content ">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <p class="page-banner__text">Thank you for your interest in our professional coaching services! We're here to
+            help you unleash your full
+            potential and achieve your goals.
+        </p>
+        <p class="page-banner__text">
+            If you have any questions, inquiries, or would like to explore how we can work together, please don't
+            hesitate to reach out. Simply fill out the form below, and we'll get back to you as soon as possible.
+        </p>
     </div>
-    <?php } ?>
+</div>
 
-    <div class="generic-content">
-        <?php the_content(); ?>
+
+<div class="contact">
+    <div class="contact-content">
+
+        <div class="generic-content">
+            <?php the_content(); ?>
+        </div>
     </div>
 
 </div>
--- single.php
+++ single.php
@@ -4,29 +4,29 @@ get_header();
 
 while (have_posts()) {
   the_post(); ?>
-  <div class="page-banner">
+<div class="page-banner page-banner--blog">
     <div class="page-banner__content ">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
 
     </div>
-  </div>
+</div>
 
-  <div class="container container--narrow page-section">
+<div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p class="metabox-wrapper">
-        <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
-          <i class="fa fa-home" aria-hidden="true"></i> Blog Home
-        </a>
-        <span class="metabox__main">Posted by
-          <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
-          <?php echo get_the_category_list(', '); ?>
-        </span>
-      </p>
+        <p class="metabox-wrapper">
+            <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Blog Home
+            </a>
+            <span class="metabox__main">Posted by
+                <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
+                <?php echo get_the_category_list(', '); ?>
+            </span>
+        </p>
     </div>
 
     <div class="generic-content"><?php the_content(); ?></div>
 
-  </div>
+</div>
 
 
 
